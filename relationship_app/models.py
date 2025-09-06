b211bcc303060a942cfd8367d45f84ac94603c63
Bem132833 <bemigbaryehuwalawork@gmail.com>
2025-09-06 18:46:32 +0300

    feat: add relationship_app with models and queries

--- relationship_app/models.py
+++ relationship_app/models.py
@@ -6,12 +6,14 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
@@ -20,13 +22,12 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
-
-from django.db import models
 
 # Create your models here.
--- relationship_app/query_samples.py
+++ relationship_app/query_samples.py
@@ -1,6 +1,20 @@
 from relationship_app.models import Author, Library
-author = Author.objects.get(name="Author Name")
-books_by_author = author.book_set.all()
-library = Library.objects.get(name="Library Name")
-books_in_library = library.books.all()
-librarian = library.librarian
+def get_books_by_author(author_name):
+    """
+    Return a QuerySet of books written by the author with the given name.
+    Example: get_books_by_author("J.K. Rowling")
+    """
+    return Author.objects.get(name=author_name).book_set.all()
+def get_books_in_library(library_name):
+    """
+    Return a QuerySet of books in the library with the given name.
+    Example: get_books_in_library("Central Library")
+    """
+    return Library.objects.get(name=library_name).books.all()
+
+def get_librarian_for_library(library_name):
+    """
+    Return the Librarian instance for the given library name.
+    Example: get_librarian_for_library("Central Library")
+    """
+    return Library.objects.get(name=library_name).librarian
